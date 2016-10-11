9213b32e4039c917187f5cc0e238a6ac9978ad91
Rebecca Post <beccanp@comcast.net>
2016-10-10 21:39:58 -0500

    Forgot to add files to commit; HW1 exercise 3 and readme updated

--- hw1/readme.md
+++ hw1/readme.md
@@ -11,11 +11,21 @@ The data was moved to this directory by:
 ```r
 cp -R hw1-snaqTimeTests ~/stat679/homework/stat679work/hw1
 ```
+
 Homework1 has 3 exercises. (2 scripts written as of 9/28)
 1. Exercise1: normalizeFileNames.sh
 This exercise normalized the filenames so that the numbers went from 01-13, instead of 1-13.
 2. Exercise2: summarizeSNaQres.sh
 This exercise creates a table containing the allowable number of hybridizations in each analysis and the elapsed time of each anaylsis. This outputs a csv file titled summarizeSNaQres.csv.
+3. Exercise3: summarizeSNaQres.sh
+This exercise creates a table, just like exercise 2, but includes more columns with further information about the analyses. The output is a csv file titled summarizeSNaQres.csv.
+
+NOTE: Exercise2 and Exercise3 have the same file name. This means this script will no longer produce the output of Exercise2. The script has been preserved both by copying the file to another name (summarizeSNaQresOG.sh), and by saving the version in git before it was altered.
+ The version was saved on 10/4:
+ ```r
+ git tag v1.2
+ git push origin v1.2
+ ```
 
 Important for running script files:
 1. All exercises assume you are in the same directory as the hw1-snaqTimeTests folder (the data folder), NOT IN hw1-snaqTimeTests. The scripts access the log and out folders under this assumption. The data folder must be called hw1-snaqTimeTests, or the script will not run either.
--- hw1/summarizeSNaQres.sh
+++ hw1/summarizeSNaQres.sh
@@ -1,25 +1,84 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+
 # Program name: summarizeSNaQres
 # Written by: Rebecca Post
-# The purpose of this script is to create a table with relevant information about our analyses.
-# The table has the name of the analysis, the number of hybridizations allowed, and the time
-# the analysis took. The table is saved in summarizeSNaQres.csv.
+# The purpose of this script is to create a table with relevant information about our analyses,
+# including the name of the analysis, the time it took to complete, the seed, some tuning
+# parameters, and number of runs that returned a score under a certain threshold.
+# The table is saved in summarizeSNaQres.csv.
 
 #make sure you run the script from the same directory that the data folder is located in.
 
 #create headers for columns in table
-echo analysis,h,CPUtime > summarizeSNaQres.csv
+echo analysis,h,CPUtime,Nruns,Nfail,fabs,frel,xabs,xrel,seed,under3460,under3450,under3440 > summarizeSNaQres.csv
 
 cd hw1-snaqTimeTests/log
 for filename in *.log
   do
-    #extract analysis name
-    #would not work if there is another o or l in the filename.
-    rootname="$(echo $filename | grep -o '[^\.log]*')"
-    #extract number of hybridizations from log file.
-    h="$(grep "hmax =" ${rootname}.log | grep -o "[0-9]*")"
-    #extract elapsed time of analysis from out file.
-    CPUtime="$(grep "Elapsed time" ../out/${rootname}.out | grep -Eo "[0-9]+\.[0-9]+")"
+    #extract directory
+    directory="$(dirname $(pwd))"
 
-    #write analysis name, number of hybridizations, and elapsed time to table file
-    echo ${rootname},${h},${CPUtime} >> ../../summarizeSNaQres.csv
+    #extract analysis name
+    rootname="$(basename $filename .log)"
+
+    #extract number of hybridizations from log file.
+    h="$(sed -E -n 's/ hmax = ([0-9]),/\1/p' ${rootname}.log)"
+
+    #extract elapsed time of analysis from out file.
+    CPUtime="$(sed -E -n 's/ Elapsed time: ([0-9]+\.[0-9]+) seconds.*/\1/p' ${directory}/out/${rootname}.out)"
+
+    #extract number of runs
+    Nruns="$(sed -E -n 's/BEGIN: ([0-9]+) runs.*/\1/p' ${directory}/log/${rootname}.log)"
+
+    #extract max number of failed proposals
+    Nfail="$(sed -E -n 's/ max number of failed proposals = ([0-9]+), .*/\1/p' ${directory}/log/${rootname}.log)"
+
+    #extract absolute tolerated difference of score function
+    fabs="$(sed -E -n 's/.*ftolAbs=([0-9]+.*[0-9]+),/\1/p' ${directory}/log/${rootname}.log)"
+
+    #extract relative tolerated difference of score function
+    frel="$(sed -E -n 's/.*ftolRel=([0-9]+.*[0-9]+), ftolAbs.*/\1/p' ${directory}/log/${rootname}.log)"
+
+    #extract xtolAbs
+    xabs="$(sed -E -n 's/xtolAbs=([0-9]+\.[0-9]+),.*/\1/p' ${directory}/log/${rootname}.log)"
+
+    #extract xtolRel
+    xrel="$(sed -E -n 's/.*xtolRel=([0-9]+\.[0-9]+)\./\1/p' ${directory}/log/${rootname}.log)"
+
+    #extract main seed (seed for the first run)
+    seed="$(sed -E -n 's/main seed ([0-9]+)/\1/p' ${directory}/log/${rootname}.log)"
+
+    #find number of runs that returned a network score better than a certain threshold
+    under3460=0
+    under3450=0
+    under3440=0
+    #extract -loglik values for each run
+    #note: numbers were cut off at the decimal place in order to be processed as an integer
+    #this does not affect the categories the values are assigned to because we used lt
+    loglik2="$(sed -E -n 's/.*\-loglik ([0-9]+)\..*/\1/p' ${directory}/out/${rootname}.out)"
+
+    #compare each -loglik value to the different thresholds
+    for value in $loglik2
+    do
+      if [ $value -lt 3460 ]
+      then
+        ((++under3460))
+        fi
+
+      if [ $value -lt 3450 ]
+      then
+        ((++under3450))
+      fi
+
+      if [ $value -lt 3440 ]
+      then
+        ((++under3440))
+      fi
+    done
+
+    #write the extracted values to a csv file
+    echo $rootname,$h,$CPUtime,$Nruns,$Nfail,$frel,$xabs,$xrel,$seed,$under3460,$under3450,$under3440>> ../../summarizeSNaQres.csv
   done
